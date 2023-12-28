552eae04506751fa776b5d2413db4c86079cb895
Maria <mariya-ivanova@list.ru>
2023-12-28 20:11:19 +0200

    New console was added

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Insert number: ");
+int number = int.Parse(Console.ReadLine());
+if (number % 2 == 0)
+ {
+   Console.WriteLine($"{number} is even");
+ }
+ else
+ {
+   Console.WriteLine($"{number} is odd"); 
+ }
